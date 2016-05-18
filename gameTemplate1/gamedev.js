bdc9c55e370fdab159004f41db71c4d5dac823a0
Thomas <tjcernosia@gmail.com>
2016-05-18 13:50:18 +0800

    Added JSTexture folder, Stretched player icon

--- gameTemplate1/gamedev.js
+++ gameTemplate1/gamedev.js
@@ -1,5 +1,12 @@
 $( document ).ready(function() {
 
-//Your jquery goes here
+
 
 });
+
+
+function movement(){
+	var evtobj=window.event? event : e;
+	var unicode=evtobj.charCode? evtobj.charCode : evtobj.keyCode;
+	var actualkey=String.fromCharCode(unicode);		
+}
