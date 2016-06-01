bc06b7b4b99a6ba21f17d116db4bfd03ac5107bd
Thomas <tjcernosia@gmail.com>
2016-06-01 09:20:58 +0800

    Player movement, generate() fixing
    Added player movement however still needs fixing, also began adding the
    distance formula to generate() to stop the rocks being to close.

--- gameTemplate1/gamedev.js
+++ gameTemplate1/gamedev.js
@@ -3,57 +3,89 @@ var generator = Math.floor((Math.random()*464)+16);
 $( document ).ready(function() {
 	generate();
 
-boolean up;
-boolean down;
-boolean left;
-boolean right;
-
-var x = $("#player").width;
+	function right(){
+		//.animate to more right
+	}
+	function left(){
+		//.animate to move left
+	}
+	function up(){
+		//.animate to move up
+	}
+	function down(){
+		//.animate to move down
+	}
 
 });
+
+var right =0; 
+var left =0;
+var up =0;
+var down =0;
+
+var ronetop;
+var rtwotop;
+var rthreetop;
+var roneleft;
+var rtwoleft;
+var rthreeleft;
 
 document.addEventListener("keydown", function(event){
 	if(event.code === "KeyD") {
 		console.log("Move Right")
-		right = true;
+		$("#player").stop().animate({
+            left: '+=32'
+        }); //move right
 	}
 	if(event.code === "KeyW") {
 		console.log("Move Up")
-		up = true;
+		$("#player").stop().animate({
+            top: '-=32'
+        });
 	}
 	if(event.code === "KeyA") {
 		console.log("Move Left")
-		left = true;
+		$("#player").stop().animate({
+            left: '-=32'
+        });
 	}
 	if(event.code === "KeyS") {
 		console.log("Move Down")
-		down = true;
+		$("#player").stop().animate({
+            top: '+=32'
+        });
 	}
 });
 
 function generate(){
+	ronetop = generator;
 	$("#rockone").css({"top": generator});
 	console.log("rockone top: "+generator);
 	generator = Math.floor((Math.random()*304)+16); 
+	roneleft = generator;
 	$("#rockone").css({"left": generator});
 	console.log("rockone left: "+generator);
 	generator = Math.floor((Math.random()*464)+16); 	
 
+	rtwotop = generator;
 	$("#rocktwo").css({"top": generator});
 	console.log("rocktwo top: "+generator);
 	generator = Math.floor((Math.random()*304)+16); 
+	rtwoleft = generator;
 	$("#rocktwo").css({"left": generator});
 	console.log("rocktwo left: "+generator);
 	generator = Math.floor((Math.random()*464)+16); 
 
+	rthreetop = generator;
 	$("#rockthree").css({"top": generator});
 	console.log("rockthree top: "+generator);
 	generator = Math.floor((Math.random()*304)+16); 
+	rthreeleft = generator; 
 	$("#rockthree").css({"left": generator});
 	console.log("rockthree left: "+generator);
-	generator = Math.floor((Math.random()*464)+16); 	
+	generator = Math.floor((Math.random()*464)+16); 
 }
 
-while (up = true) {
+	
 
-}
+
