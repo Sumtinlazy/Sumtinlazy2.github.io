a5db1a6d910d969ccfe12578cb439aa626dd7a78
Thomas <tjcernosia@gmail.com>
2016-06-07 09:44:46 +0800

    Fixes, updates

--- gameTemplate1/gamedev.js
+++ gameTemplate1/gamedev.js
@@ -1,6 +1,14 @@
 var generator = Math.floor((Math.random()*464)+16);
 
 $( document ).ready(function() {
+
+	var elem = document.getElementById("#player");
+	var leftvar;
+	var topvar;
+
+	elem.style.left = leftvar;
+	elem.style.top = topvar;
+
 	generate();
 
 	function right(){
@@ -16,6 +24,36 @@ $( document ).ready(function() {
 		//.animate to move down
 	}
 
+	console.log("left " + leftvar);
+	console.log("top " + topvar);
+
+	document.addEventListener("keydown", function(event){
+	if(event.code === "KeyD") {
+		console.log("Move Right");
+		$("#player").stop().animate({
+            left: '+=32'
+        }); //move right
+	}
+	if(event.code === "KeyW") {
+		console.log("Move Up");
+		$("#player").stop().animate({
+            top: '-=32'
+        });
+	}
+	if(event.code === "KeyA") {
+		console.log("Move Left");
+		$("#player").stop().animate({
+            left: '-=32'
+        });
+	}
+	if(event.code === "KeyS") {
+		console.log("Move Down");
+		$("#player").stop().animate({
+            top: '+=32'
+        });
+	}
+});
+
 });
 
 var right =0; 
@@ -30,32 +68,7 @@ var roneleft;
 var rtwoleft;
 var rthreeleft;
 
-document.addEventListener("keydown", function(event){
-	if(event.code === "KeyD") {
-		console.log("Move Right")
-		$("#player").stop().animate({
-            left: '+=32'
-        }); //move right
-	}
-	if(event.code === "KeyW") {
-		console.log("Move Up")
-		$("#player").stop().animate({
-            top: '-=32'
-        });
-	}
-	if(event.code === "KeyA") {
-		console.log("Move Left")
-		$("#player").stop().animate({
-            left: '-=32'
-        });
-	}
-	if(event.code === "KeyS") {
-		console.log("Move Down")
-		$("#player").stop().animate({
-            top: '+=32'
-        });
-	}
-});
+var health = 100;
 
 function generate(){
 	ronetop = generator;
@@ -86,6 +99,28 @@ function generate(){
 	generator = Math.floor((Math.random()*464)+16); 
 }
 
+function enemy(){
+	if(($('#nmeone').offset().top - $('#player').offset().top) > 0) {
+		$("#nmeone").stop().animate({
+            top: '-=30'
+        });
+	} else {
+		$("#nmeone").stop().animate({
+            top: '+=30'
+        });
+	}
+
+	if(($("#nmetwo").offset().left - $("#player").offset().left) > 0) {
+	
+	$("#nmeone").stop().animate({
+            left: '-=30'
+        });
+	} else {
+		$("#nmeone").stop().animate({
+            left: '+=30'
+        });
+	}
+}
 	
 
 
